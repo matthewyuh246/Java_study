f6da1edab7b92fa763d2facb4510623cfcd83ae4
Yuhri <matthewyuh246@gmail.com>
2024-04-14 11:22:00 +0900

    first

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("githubにアップロードする。");
+        int val = 0827;
+        boolean flg = true;
+        if(flg == true) {
+            do {
+                System.out.println(val);
+            } while (val > 10);
+        }
     }
 }
